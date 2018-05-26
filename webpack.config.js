5e83b90cec2ea2358d86701db2cdc95ed55baf81
Catherine Ligad <kateligad@gmail.com>
2018-05-26 12:19:53 -0700

    more setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, '/client'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
