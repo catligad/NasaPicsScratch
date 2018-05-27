df8a5d4dd4df30709bf6ad64cf8f96137b733d78
Catherine Ligad <kateligad@gmail.com>
2018-05-26 17:13:07 -0700

    able to get the data needed from the query to the api

--- server/getHelper.js
+++ server/getHelper.js
@@ -2,13 +2,15 @@ const axios = require('axios');
 const key = require('./apiKey');
 axios.defaults.headers.common['Authorization'] = key;
 
-const helper = (query) => {
+//------ this is making a get request to the NASA API
+module.exports = (query) => {
   return axios({
     url: `/search?q=${query}`, 
     method: 'get',
     baseURL: 'https://images-api.nasa.gov',
     contentType: 'application/x-www-form-urlencoded',
+    params:{
+      media_type: 'image',
+    }
   })
 }
-
-module.exports = helper;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
-const helper = require('./fetchHelper')
+const helper = require('./getHelper')
 const app = express();
 
 app.use(express.static(path.join(__dirname + '/../dist')));
@@ -9,8 +9,15 @@ app.use(parser.text());
 
 app.post('/server', (req, res) => {
   helper(req.body).then(res => {
-    console.log(res)
+    // console.log(res.data.collection.items[0].links[0].href)
+    for (let i = 0; i < res.data.collection.items.length; i++) {
+      const image = res.data.collection.items[i].links[0].href;
+
+
+
+    }
   })
+  res.send({result: 'I got you boo'})
 })
 
 app.listen(3000, () => console.log('HELLO!'));
